e0bb5894862f111895b33a74c96ee24a765b80c8
Danny Torkelson <dtorkelson@gmail.com>
2018-06-01 20:25:14 -0400

    Flesh out dataframe, sum numeric columns

--- temp.py
+++ temp.py
@@ -11,6 +11,22 @@ import matplotlib.pyplot as plt
 
 s = pd.Series([1,3,5,np.nan,6,8])
 
-dates = pd.date_range('20130101', periods=6)
+user_id = np.random.randint(123456, 999999, size=300)
 
-df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=list('ABCD'))
+names = pd.read_excel('names.xlsx', squeeze=True)
+countries = pd.read_excel('names.xlsx', 1, squeeze=True)
+
+df = pd.DataFrame(np.random.rand(len(names), 4), columns=list('ABCD'))
+df['name'] = names
+df['user_id'] = np.random.choice(user_id, size=len(names))
+df['A_text'] = np.random.choice(countries, size=len(names))
+cols = ['name', 'user_id', 'A_text'] + list('ABCD')
+df = df[cols]
+
+# Add up numbers
+sumlist = list('ABCD')
+df['sum'] = df[sumlist].sum(axis=1)
+
+
+
+print(df.head())
