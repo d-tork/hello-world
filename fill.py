b7159cab6171cdfd679e57a6fa6c646953e2d1ff
d-tork <dtorkelson@gmail.com>
2018-06-05 23:17:39 -0400

    Comments galoreˆ

--- fill.py
+++ fill.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 
 def fill_readme(sheet):
+    """ Read individual lines from text file into list. """
     txtlist = []
     with open('poc_info.txt') as f:
         for row in f:
@@ -18,34 +19,42 @@ def fill_readme(sheet):
 
 
 def write_workbook(org):
+    # Record in log which org is being written
     with open(logfile, append_write) as writer:
         writer.write(org + '\n')
+
+    # Copy the template, rename it, set as active xlwings workbook
     outfile = os.path.join('output', '18q3 Roster - {}.xlsx'.format(org))
     copy2('my_template.xlsx', outfile)
     wb = xw.Book(outfile)
     print('Opening:', wb.fullname)
 
+    # Filter full dataframe for member, obtain unique sheet names
     df1 = wfa.loc[wfa.org == org]
     sht_list = df1.sheet.unique()
     print('# of sheets: {}'.format(len(sht_list)))
     existing_sheets = list(wb.sheets)[1:-1]
+
+    # By sheet, filter org df and write out to Excel worksheet
     for sheet, sht in zip(sht_list, existing_sheets):
         sht.name = sheet
         df2 = df1.loc[df1.sheet == sheet]
         sht.range('A5').options(index=False, header=False).value = df2
         print('\tWrote: {} {} rows'.format(sht.name, len(df2)))
-        with open(logfile, append_write) as writer:
+        with open(logfile, append_write) as writer:  # log it
             writer.write('\t' + sheet + str(len(df2)) + '\n')
+
+    # Write sheet list to summary tab table, write POC to README
     summary = wb.sheets('Player Summary')
     summary.range('Q2').options(transpose=True).value = sht_list
     fill_readme(wb.sheets['README'])
 
-    # delete unused sheets
+    # Delete unused sheets
     wfa_sheets = [x for x in wb.sheets if 'WFA' in str(x)]
     for sht in wfa_sheets:
         sht.delete()
 
-    # Reset active
+    # Reset active sheet to first roster
     wb.sheets[1].activate()
 
     wb.save()
@@ -78,12 +87,14 @@ try:
     app1 = xw.apps[0]
 except IndexError:
     app1 = xw.App(visible=False)
-app1.screen_updating = True  # faster; don't show what it's doing
+app1.screen_updating = True  # false is faster, but if error then you can't see
 
 # choice = int(input('Which index for org? : '))
 
+# MAIN EVENT
 for org in wfa_org_list:
     write_workbook(org)
+
 app1.quit()
 
 end = datetime.now()
